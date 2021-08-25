4d7fa7e65fb0ec72f2194be904a005004688ecc0
Alvaro Reig <alvaroreig@gmail.com>
2021-08-25 11:46:28 +0200

    after new user form

--- src/main/java/com/example/servingwebcontent/GreetingController.java
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.servingwebcontent.entities.User;
@@ -31,5 +33,18 @@ private UserRepository userRepository;
 		model.addAttribute("users",(List<User>) userRepository.findAll());
 		return "users";
 	}
+	
+	@GetMapping("/user")
+	  public String greetingForm(Model model) {
+	    model.addAttribute("user", new User());
+	    return "user";
+	  }
+
+	  @PostMapping("/user")
+	  public String userSubmit(@ModelAttribute User user, Model model) {
+		userRepository.save(user);
+	    model.addAttribute("user", user);
+	    return "userResult";
+	  }
 
 }
