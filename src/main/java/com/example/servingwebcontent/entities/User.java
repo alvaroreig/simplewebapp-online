f6dbc9b2703103d6319a2a6974f956f4e99e4064
Alvaro Reig <alvaroreig@gmail.com>
2021-08-25 13:12:46 +0200

    readme stub and new User constructor

--- src/main/java/com/example/servingwebcontent/entities/User.java
+++ src/main/java/com/example/servingwebcontent/entities/User.java
@@ -15,7 +15,15 @@ public class User {
 
   private String email;
 
-  public Integer getId() {
+  public User() {}
+  
+  public User(int id, String name, String email) {
+	this.id = id;
+	this.name = name;
+	this.email = email;
+}
+
+public Integer getId() {
     return id;
   }
 
