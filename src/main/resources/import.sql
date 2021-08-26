9eff020d5a1c8408a02f9016ef3f9780749179a5
Alvaro Reig <alvaroreig@gmail.com>
2021-08-26 11:43:08 +0200

    strating with fresh data on startup

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO user (id,name,email) VALUES (1,'Hortensia','hortensia@acme.com');
+INSERT INTO user (id,name,email) VALUES (2,'Inma','inma@acme.com');
+INSERT INTO user (id,name,email) VALUES (3,'Jesús','jesus@acme.com');
+INSERT INTO user (id,name,email) VALUES (4,'Antonio','antonio@acme.com');
+INSERT INTO user (id,name,email) VALUES (5,'Luis','luis@acme.com');
+INSERT INTO user (id,name,email) VALUES (6,'David','david@acme.com');
+INSERT INTO user (id,name,email) VALUES (7,'Felipe','felipe@acme.com');
