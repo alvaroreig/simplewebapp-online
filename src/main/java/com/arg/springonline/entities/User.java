c02d2f16e833b14655e56705c98a54cf2a5395d8
Alvaro Reig <alvaroreig@gmail.com>
2021-08-26 09:30:34 +0200

    refactor

--- src/main/java/com/arg/springonline/entities/User.java
+++ src/main/java/com/arg/springonline/entities/User.java
@@ -7,42 +7,43 @@ import javax.persistence.Id;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
-  @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
-  private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
 
-  private String name;
+	private String name;
 
-  private String email;
+	private String email;
 
-  public User() {}
-  
-  public User(String name, String email) {
-	this.name = name;
-	this.email = email;
-}
+	public User() {
+	}
 
-public Integer getId() {
-    return id;
-  }
+	public User(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
+	public Integer getId() {
+		return id;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public String getEmail() {
-    return email;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
