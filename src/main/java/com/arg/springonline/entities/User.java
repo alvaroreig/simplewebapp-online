83c7b407ca32d874dfab8dcde67d9bb67d93fce7
Alvaro Reig <alvaroreig@gmail.com>
2021-08-26 11:17:10 +0200

    logging

--- src/main/java/com/arg/springonline/entities/User.java
+++ src/main/java/com/arg/springonline/entities/User.java
@@ -46,4 +46,9 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	@Override
+	public String toString() {
+		return id.toString() + ":" + name + ":" + email;
+	}
 }
