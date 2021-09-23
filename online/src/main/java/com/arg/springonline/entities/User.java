b2f0b93b8c1d863219144a50279bd2944e391a06
Alvaro Reig <alvaroreig@gmail.com>
2021-09-23 09:35:43 +0200

    JDBC version of GET users

--- online/src/main/java/com/arg/springonline/entities/User.java
+++ online/src/main/java/com/arg/springonline/entities/User.java
@@ -18,6 +18,12 @@ public class User {
 	public User() {
 	}
 
+	public User(Integer id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
 	public User(String name, String email) {
 		this.name = name;
 		this.email = email;
